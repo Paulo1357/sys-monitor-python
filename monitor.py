a9377607b62a158f450732368fdbfe1c01973a34
Paulo1357 <5raccon@gmail.com>
2026-03-15 10:36:45 -0300

    feat: adiciona monitoramento de memória RAM e requirements

--- monitor.py
+++ monitor.py
@@ -1,16 +1,19 @@
 import platform
 import datetime
 import os
+import psutil  # Nossa nova ferramenta
 
 def gerar_relatorio():
-    # Coletando os dados do seu Linux
     sistema = platform.system()
     versao = platform.release()
     arquitetura = platform.machine()
     usuario = os.getlogin()
     data_atual = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    
+    # Capturando uso de memória RAM (Dinâmico)
+    memoria = psutil.virtual_memory()
+    uso_ram = memoria.percent
 
-    # Formatando a mensagem (Template de Log)
     relatorio = (
         f"--- RELATÓRIO DE SISTEMA ---\n"
         f"Data/Hora: {data_atual}\n"
@@ -18,14 +21,14 @@ def gerar_relatorio():
         f"SO: {sistema}\n"
         f"Kernel: {versao}\n"
         f"Arquitetura: {arquitetura}\n"
+        f"Uso de RAM: {uso_ram}%\n"
         f"---------------------------\n"
     )
 
-    # Gravando no arquivo (Modo 'a' de append para não apagar o anterior)
     with open("log_sistema.txt", "a") as arquivo:
         arquivo.write(relatorio)
-
-    print(f"✅ Relatório salvo com sucesso para o usuário {usuario}!")
+    
+    print(f"✅ Relatório atualizado com sucesso! (RAM: {uso_ram}%)")
 
 if __name__ == "__main__":
     gerar_relatorio()
